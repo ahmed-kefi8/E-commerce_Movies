22a0dfadafcb720abc75d0dbbf480b3f44ad32f2
ahmedkefi <ahmedkefi.ing@gmail.com>
2016-04-27 21:49:23 +0100

    Finished the cart controller and view , everything is working nicely

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -3,8 +3,12 @@ Movie_Store_App.controller('CartController',['$scope','CartService', function($s
 
 $scope.cart = CartService.cart;
 
+$scope.deleteFromCart = function(){
+	CartService.deleteFromCart($scope.cart.indexOf($scope.movie));
+}
 
-
-
+$scope.order = function(){
+	alert('YOUR ORDER HAS BEEN PLACED, THANK YOU');
+}
 
 	}]);
