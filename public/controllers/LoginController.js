e94cbe7416f44595de8f2df964e1a31807d0e879
ahmedkefi <ahmedkefi.ing@gmail.com>
2016-05-02 08:55:39 +0100

    SignUp and Login working, checking email and password from MongoDB users

--- public/controllers/LoginController.js
+++ public/controllers/LoginController.js
@@ -1,30 +1,44 @@
 
 Movie_Store_App.controller('LoginController',['$scope','$location','$rootScope','UserLoginFactory', function($scope, $location, $rootScope, UserLoginFactory){
-$scope.user = {};
+
 
 
 
 $scope.submitLogin = function(){
 
-console.log("submit login fired");
+
+//console.log("submit login fired");
+//console.log("submitted email  :  " + $scope.submittedEmail);
+//console.log("submitted password  :  " + $scope.submittedPassword);
+
+
+//$scope.user = UserLoginFactory.get({ email: $scope.submittedEmail }); //Get a single user
+
+
+UserLoginFactory.get({ email: $scope.submittedEmail }, function(data) {
+	$scope.user = data;
+	//console.log($scope.user.Password);
 
 
 
 
-$scope.user = UserLoginFactory.get({ email: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" }); //Get a single user
- console.log($scope.user);
 
 
-if ($scope.user.password != "0")
-{   $rootScope.loggedIn = true;
-	$location.path('/Cart');}
+console.log($scope.user);
+
+if ($scope.submittedPassword.toString() === $scope.user.Password.toString())
+	{   $rootScope.loggedIn = true;
+		$location.path('/Cart');}
+
+		else
+			{alert('Wrong email or password');}
 
 
-else
-{alert('Wrong email or password');}
 
+}); 
 
 };
+
 
 }]);
 
